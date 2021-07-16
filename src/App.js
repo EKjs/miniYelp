d1bb41ae3fb888d5dfdea324edd5b20ab5ed456e
EKjs <doomedarchnagel@gmail.com>
2021-07-16 09:24:59 +0200

    no warnings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route,Redirect } from "react-router-dom";
 //import 'bootstrap/scss/bootstrap.scss';
-import SearchBar from "./components/SearchBar";
+import MainPage from "./components/MainPage";
 import Restaurants from "./components/Restaurants";
 import RestaurantInfo from "./components/RestaurantInfo";
 import Tags from "./components/Tags";
@@ -13,7 +13,7 @@ function App() {
       <Route exact path="/">
         <Redirect to="/search" />
       </Route>
-      <Route exact path="/search" component={SearchBar}/> 
+      <Route exact path="/search" component={MainPage}/> 
       <Route exact path="/restaurants" component={Restaurants}/>
       <Route exact path="/restaurants/:restaurantId" component={RestaurantInfo}/>
       <Route exact path="/tags" component={Tags}/>
@@ -25,24 +25,3 @@ function App() {
 }
 
 export default App;
-
-{/*
-  <div className="row">
-          <div className="col">
-          </div>
-        </div>
-<Route exact path="/" component={}/> 
-<Route exact path="/restaurants" component={Restaurant}/>
-<Route exact path="/restaurants/:restaurantId" component={Restaurants}/>
-
-<Route exact path="/tags" component={Tags}/>
-<Route exact path="/tags/:tagId" component={Tag}/>
-
-<Route exact path="/cities" component={Cities}/>
-<Route exact path="/cities/:tagId" component={City}/>
-
-import { Link } from "react-router-dom";
-<Link to={`/restaurants/${id}`}>
-  blabla
-</Link>
-*/}
